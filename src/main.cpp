910fc4b9c14b1369ab71d679a9e9c1f4a5c126af
Aleph0Null0 <aryahosseini.contact@gmail.com>
2025-01-13 15:43:16 -0500

    Minor Stuff

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,19 @@
 // Thermistor parameters from the datasheet
 #define RTreference 10000
 #define B 3988
+// Series resistor value = 10 kΩ
+#define R 10000
 #define num_thermistors 5
 
+#define HEATER_PIN D5
+#define COOLER_PIN D6
 constexpr int THERMISTOR_PINS[num_thermistors] = {A0, A1, A2, A3, A4};
+
 float TEMPERATURE_ARRAY[num_thermistors] = {0, 0, 0, 0, 0};
 int COUNTER_ARRAY[num_thermistors] = {0, 0, 0, 0, 0};
 String TEMPERATURE_LABELS[num_thermistors] = {"HEATER", "COOLER", "OUTSIDE", "ELECTRONICS", "INSIDE"};
-// Our series resistor value = 10 kΩ
-#define R 10000
 
-// Variables for calculations
+// Variables for thermistor calculation
 float thermistor_resistance, resistor_voltage, ln, Tx, T0, thermistor_voltage;
 
 void setup() {
@@ -21,10 +24,10 @@ void setup() {
     T0 = 25 + 273.15;
 }
 
-float get_temperature(int thermistor_pin) {
-    float thermistor_voltage = (5.00 / 1023.00) * analogRead(thermistor_pin);
-    float resistor_voltage = 5.00 - thermistor_voltage;
-    float thermistor_resistance = thermistor_voltage / (resistor_voltage / R);
+float get_temperature(const int thermistor_pin) {
+    const float thermistor_voltage = (5.00 / 1023.00) * analogRead(thermistor_pin);
+    const float resistor_voltage = 5.00 - thermistor_voltage;
+    const float thermistor_resistance = thermistor_voltage / (resistor_voltage / R);
     ln = log(thermistor_resistance / RTreference);
     return (1 / ((ln / B) + (1 / T0))) - 273.15;
 }
@@ -35,7 +38,7 @@ void get_temperature_array(int array_size, const int pin_array[]) {
     }
 }
 
-void update_counter_array(int array_size) {
+void update_counter_array(const int array_size) {
     for (int i = 0; i < array_size; i++) {
         if (TEMPERATURE_ARRAY[i] >= 35) { COUNTER_ARRAY[i]++;}
         else if (TEMPERATURE_ARRAY[i] <= 27) { COUNTER_ARRAY[i]--;}
@@ -46,7 +49,9 @@ void update_counter_array(int array_size) {
     }
 }
 
-void start_heating() {}
+void start_heating() {
+
+}
 void start_cooling() {}
 
 
@@ -54,6 +59,7 @@ void loop() {
     get_temperature_array(num_thermistors, THERMISTOR_PINS);
     update_counter_array(num_thermistors);
     //TODO: Implement evaluation of counter array, and adjust signal to heater/cooler accordingly
+
     if (COUNTER_ARRAY[0] > 0) {start_heating();}
     else if (COUNTER_ARRAY[0] < 0) {start_cooling();}
     //TODO: Run an atmospheric cycling routine at a given time interval
