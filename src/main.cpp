76e3997129da176e0788a46f92d9e927b01eaac4
Aleph0Null0 <64078009+Aleph0Null0@users.noreply.github.com>
2025-02-03 00:37:14 -0500

    Updated for PCB thermistors

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,52 @@
 #include <Arduino.h>
+#include <math.h>
+//#include <cmath>
+
 // Thermistor parameters from the datasheet
-#define RTreference 10000
-#define B 3988
+#define RTref 4000
+#define B 3568
 // Series resistor value = 10 kΩ
 #define R 10000
+#define T0 298.15
 #define num_thermistors 5
-
+#define instr_gain (1+(49.4/42.2))
 #define HEATER_PIN D5
 #define COOLER_PIN D6
 constexpr int THERMISTOR_PINS[num_thermistors] = {A0, A1, A2, A3, A4};
-
+const double resistance_ratio = 4.0/14.0;
 float TEMPERATURE_ARRAY[num_thermistors] = {0, 0, 0, 0, 0};
 int COUNTER_ARRAY[num_thermistors] = {0, 0, 0, 0, 0};
 String TEMPERATURE_LABELS[num_thermistors] = {"HEATER", "COOLER", "OUTSIDE", "ELECTRONICS", "INSIDE"};
 
 // Variables for thermistor calculation
-float thermistor_resistance, resistor_voltage, ln, Tx, T0, thermistor_voltage;
+float thermistor_resistance, resistor_voltage, ln, Tx, thermistor_voltage;
 
 void setup() {
     // Setup serial communication
     Serial.begin(9600);
+    digitalWrite(3, HIGH);
     // Convert T0 from Celsius to Kelvin
-    T0 = 25 + 273.15;
 }
 
 float get_temperature(const int thermistor_pin) {
     const float thermistor_voltage = (5.00 / 1023.00) * analogRead(thermistor_pin);
-    const float resistor_voltage = 5.00 - thermistor_voltage;
-    const float thermistor_resistance = thermistor_voltage / (resistor_voltage / R);
-    ln = log(thermistor_resistance / RTreference);
-    return (1 / ((ln / B) + (1 / T0))) - 273.15;
+    //Serial.print(thermistor_voltage);
+    //Serial.println();
+    const float differential_voltage = thermistor_voltage/instr_gain;
+    //Serial.print(differential_voltage);
+    //Serial.println();
+    const float resistance_factor = (differential_voltage/5) + (4.0/14);
+    /*Serial.print(resistance_ratio);
+    Serial.println();
+    Serial.print(resistance_factor);
+    Serial.println();*/
+    //const float resistor_voltage = 5.00 - differential_voltage;
+    //const float resistance_factor = RTref / (R + RTref);
+    const float thermistor_resistance = R*(resistance_factor / (1 - resistance_factor));
+    //Serial.print(thermistor_resistance);
+    //Serial.println();
+    //return (1 / ((ln / B) + (1 / T0))) - 273.15;
+    return (1 / (log(thermistor_resistance/R)/B + 1/T0)) - 273.15;
 }
 
 void get_temperature_array(int array_size, const int pin_array[]) {
@@ -40,7 +57,7 @@ void get_temperature_array(int array_size, const int pin_array[]) {
 
 void update_counter_array(const int array_size) {
     for (int i = 0; i < array_size; i++) {
-        if (TEMPERATURE_ARRAY[i] >= 35) { COUNTER_ARRAY[i]++;}
+        if (TEMPERATURE_ARRAY[i] >= 33) { COUNTER_ARRAY[i]++;}
         else if (TEMPERATURE_ARRAY[i] <= 27) { COUNTER_ARRAY[i]--;}
         else {
             if (COUNTER_ARRAY[i] > 0) { COUNTER_ARRAY[i]--; }
